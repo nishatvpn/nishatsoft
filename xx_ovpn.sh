91d46facd76b620aec1fbf5ce13bc09479896ff3
nishatvpn <92996099+nishatvpn@users.noreply.github.com>
2022-04-15 06:06:43 +0200

    Update xx_ovpn.sh

--- xx_ovpn.sh
+++ xx_ovpn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='82.223.165.66';
-USER='nishatraji_bwh';
-PASS='@@@@F1r3n3t';
-DBNAME='nishatraji_bwh';
+db_host='82.223.165.66';
+db_user='nishatad_nishat';
+db_pass='@@Nishat123';
+db_name='nishatad_nishat';
 
 install_require()
 {
